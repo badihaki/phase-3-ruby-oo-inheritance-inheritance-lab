b587d458e755b98b9b0cdf5a2fa532350cfe05df
Badi-Haki Martin <jojackblack@gmail.com>
2022-07-13 13:04:38 -0400

    commit tree 86050c63aa22a5b26fb70f59a828bf3710d1f267

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user'
+require 'pry'
 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn fact
+        @knowledge << fact
+    end
 end
